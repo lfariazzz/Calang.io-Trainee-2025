79d4a26eabdb1f50d00ef9bb1fd374e8b243cba8
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-12-03 16:22:18 -0300

    Cria rotas da API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express, { json} from 'express'
 import cors from "cors";
+import router from './routes';
 
 export function createApp(){
     const app = express();
     app.use(json());
-
+    app.use("/api", router);
+    
     const corsOptions = {
         origin: '*',
     }
---
+++ backend/src/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/movies", );
+router.get("/movies/:id",);
+router.post("/movies", );
+router.get("/movies/search",);
+router.delete("/movies/:id", );
+router.patch("/movies/:id");
+
+router.post("/contact", );
+
+export default router;
