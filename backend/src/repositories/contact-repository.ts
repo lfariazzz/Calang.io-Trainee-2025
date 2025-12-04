17411635b17019d2144bc4bb522bff7703a4daa0
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-12-04 10:39:51 -0300

    feat(contacts): Implementa banco de dados persistente para contato

--- backend/src/repositories/contact-repository.ts
+++ backend/src/repositories/contact-repository.ts
@@ -1,12 +1,12 @@
 import { contact } from "../models/contact-model";
-
-const database: contact[]=[
-
-]
+import { db, loadDB, saveDB } from "../database/db"
 
 export const findAllContacts = async(): Promise<contact[]> => {
-    return database
+    await loadDB();
+    return db.data!.contacts
 }
-export const insertContact = async(contact: contact) => {
-    database.push(contact)
+export const insertContact = async(newContact: contact) => {
+    await loadDB();
+    db.data!.contacts.push(newContact);
+    await saveDB();
 }
